2e2f6b73deb4b4bb7c85278bb77eb419a301cd0a
Dillo Raju <dilloshion@gmail.com>
2026-09-07 10:33:05 -0400

    Fix errors

--- lua/locutus/plugins/treesitter.lua
+++ lua/locutus/plugins/treesitter.lua
@@ -1,54 +1,62 @@
+local parsers = {
+  "bash",
+  "css",
+  "dockerfile",
+  "gitignore",
+  "graphql",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "prisma",
+  "python",
+  "query",
+  "ruby",
+  "svelte",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   dependencies = {
     { "windwp/nvim-ts-autotag", opts = {} },
   },
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require("nvim-treesitter")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "ruby",
-        "python",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
+    treesitter.setup()
+    treesitter.install(parsers)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function(args)
+        local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+        if lang and vim.list_contains(parsers, lang) and pcall(vim.treesitter.start, args.buf, lang) then
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
     })
+
+    -- Preserve the old incremental-selection keys using Neovim 0.12's
+    -- built-in Tree-sitter selection API.
+    vim.keymap.set("n", "<C-space>", function()
+      vim.cmd.normal({ "v", bang = true })
+      vim.treesitter.select("parent")
+    end, { desc = "Start syntax node selection" })
+    vim.keymap.set("x", "<C-space>", function()
+      vim.treesitter.select("parent")
+    end, { desc = "Select parent syntax node" })
+    vim.keymap.set("x", "<BS>", function()
+      vim.treesitter.select("child")
+    end, { desc = "Select child syntax node" })
   end,
 }
